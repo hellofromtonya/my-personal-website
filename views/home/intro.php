c3d1ab5f167e033784e23f45ec130d0b049c3a0a
hellofromtonya <hellofromtonya@knowthecode.io>
2017-08-27 14:46:00 -0500

    bug fix about/ to about

--- views/home/intro.php
+++ views/home/intro.php
@@ -17,7 +17,7 @@
                 <a href="#we">We</a>
             </li>
             <li>
-                <a href="<?php echo $site_url; ?>/about/">About</a>
+                <a href="<?php echo $site_url; ?>/about">About</a>
             </li>
             <li>
                 <a href="<?php echo $site_url; ?>/manifesto">Manifesto</a>
